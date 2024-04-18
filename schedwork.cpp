2d8fd8f974249147447ec1414baebde43ae304c8
Rishi Sujit <sujit@usc.edu>
2024-04-18 06:18:34 +0000

    hw5 submission

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,6 +22,9 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
+bool scheduleHelper (DailySchedule& sched, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, size_t row, size_t col, vector<size_t>& numShifts);
+bool DailyScheduleComplete(DailySchedule& sched);
+
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +40,53 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    //resie and initalize
+    sched.resize(avail.size(), vector<Worker_T>());
+    vector<size_t> numShifts(avail[0].size(), 0);
+    return scheduleHelper(sched, avail, dailyNeed, maxShifts, 0,0, numShifts);
 
 
 
 
+
+}
+//recursive schedule function to generate shedule
+bool scheduleHelper (DailySchedule& sched, const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts, size_t row, size_t col, vector<size_t>& numShifts)
+{
+  //base case if all days are scheduled
+  if (row == avail.size())
+  {
+    return true;
+  }
+  //iterate over shifts for current day
+  else{
+    for(size_t i = col; i <avail[0].size(); i++)
+    {
+      //check if exceeds number
+      if(avail[row][i] && numShifts[i] < maxShifts)
+      {
+        sched[row].push_back(i);
+        numShifts[i]++;
+        if(sched[row].size() == dailyNeed)
+        {
+          //assign worker to the currents day scheduled
+          if(scheduleHelper(sched, avail, dailyNeed, maxShifts, row+1, 0, numShifts))
+          {
+            return true;
+          }
+        }
+        //recursively call with the next shift for the current day
+        bool status = scheduleHelper(sched, avail, dailyNeed, maxShifts, row, i+1, numShifts);
+        if(status)
+        {
+          return true;
+        }
+        //if not backtrack
+        sched[row].pop_back();
+        numShifts[i]--;
+      }
+    }
+    return false;
+  }
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,6 +5,8 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <unordered_map>
+#include <vector>
 #endif
 
 #include "wordle.h"
@@ -13,6 +15,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelp(const std::string& in, std::string& floating, const set<string>& dict, unsigned int n, string curr, std::set<std::string>& ans, int filled);
 
 
 // Definition of primary wordle function
@@ -22,7 +25,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string>ans;
+    string floatingReference = floating;
+    int filled = in.size() - std::count(in.begin(), in.end(), '-');
+    wordleHelp(in, floatingReference, dict, 0, in, ans, filled);
+    return ans;
 
 }
+//helper function to find words
+void wordleHelp(const std::string& in, std::string& floating, const set<string>& dict, unsigned int n, string curr, std::set<std::string>& ans, int filled)
+{
+    //base case if word is complete
+    if(n == curr.size())
+    {
+        //check if word is in dictionary
+        if(!(dict.find(curr) != dict.end()))
+        {
+            return;
+        }
+        else if(floating.empty())
+        {
+            ans.insert(curr);
+            return;
+        }
+        return;
+    }
+    //base case if it exceeds space return
+    if(floating.size() + filled > (in.size()-n))
+    {
+        return;
+    }
+    
+    if(curr[n] != '-')
+    {
+        wordleHelp(in, floating, dict, n+1, curr, ans, filled-1);
+    }
+    else
+    {
+        //iterate over letters
+        for(char letter = 'a'; letter <= 'z'; ++letter)
+        {
+            size_t position = floating.find(letter);
+            if(position != std::string::npos)
+            {
+                string newFloatingLetter = floating;
+                newFloatingLetter.erase(position, 1);
+                curr[n] = letter;
+                wordleHelp(in, newFloatingLetter, dict, n+1, curr, ans, filled);
+            }
+            curr[n] = letter;
+            //recur with unchanged floating laters
+            wordleHelp(in, floating, dict, n+1, curr, ans, filled);
+        }
+    }
+}
 
-// Define any helper functions here
+
